443720f13ae21bd8c2f2bf1ac3a6b3f315c6a64a
tfaculty <tfaculty@LT409.allianceblr.com>
2024-08-22 13:21:31 +0530

    core java program SI CI first comment

---
+++ src/corej/java1.java
@@ -0,0 +1,34 @@
+package corej;
+import java.util.Scanner;
+public class java1 
+{
+double p, t, r;
+double si, ci;
+Scanner s;
+	void input()
+	{	s=new Scanner(System.in);
+		System.out.println("Enter prinicpal Amount");
+		p=s.nextDouble();
+		System.out.println("Enter no of years");
+		t=s.nextDouble();
+		System.out.println("Enter Rate of Interest");
+		r=s.nextDouble();
+	}
+	void compute()
+	{	si=(p*t*r)/100;
+		ci=p*Math.pow(1+(r/100),t);
+	}
+	void display()
+	{	System.out.println("Simple Interest is:"+si);
+		System.out.println("compound Interest is:"+ci);
+	}
+	public static void main(String[] args) 
+	{
+		java1 j=new java1();
+		j.input();
+		j.compute();
+		j.display();
+
+	}
+
+}
